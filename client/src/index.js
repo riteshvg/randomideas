6634a8867dd82f788ce132ed4a824e89f4b7e38e
Ritesh Virendra Gupta <riteshg@Riteshs-MacBook-Powerhouse.local>
2025-06-07 15:52:15 +0530

    added modal.js and form submit feature

--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,8 @@
 import '@fortawesome/fontawesome-free/css/all.css';
 import Modal from './components/Modal';
+import IdeaForm from './components/IdeaForm';
 import './css/style.css';
 
 const modal = new Modal();
-
-// const modal = document.querySelector('#modal');
-// const modalBtn = document.querySelector('#modal-btn');
-
-// function open() {
-//   modal.style.display = 'block';
-// }
-// function close() {
-//   modal.style.display = 'none';
-// }
-// function outsideClick(e) {
-//   if (e.target === modal) {
-//     close();
-//   }
-// }
-
-// modalBtn.addEventListener('click', open);
-// window.addEventListener('click', outsideClick);
+const ideaForm = new IdeaForm();
+ideaForm.render();
