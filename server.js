0fae90c85da1727fc8dd9df3fb2d83037f2b4425
Ritesh Virendra Gupta <riteshg@Riteshs-MacBook-Powerhouse.local>
2025-06-07 06:00:34 +0530

    initial build

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+require('dotenv').config();
+const port = process.env.PORT || 5001;
+const connectDB = require('./config/db');
+
+connectDB()
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch((err) => console.error('MongDB conenction error', err));
+
+const app = express();
+
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extende: false }));
+
+//Home
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to RandomIdeas' });
+});
+
+const ideasRouter = require('./routes/ideas');
+app.use('/api/ideas', ideasRouter);
+
+app.listen(port, () => console.log(`Server listening on port: ${port}`));
